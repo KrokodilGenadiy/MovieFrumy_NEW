f12cc0e6b56490e666128b145ea66bfb0899067b
KrokodilGenadiy <alex.usevich@gmail.com>
2023-01-01 12:11:41 +0300

    added modules to AppComponent

--- app/src/main/java/com/zaus_app/moviefrumy_new/di/AppComponent.kt
+++ app/src/main/java/com/zaus_app/moviefrumy_new/di/AppComponent.kt
@@ -1,4 +1,15 @@
 package com.zaus_app.moviefrumy_new.di
 
+import com.zaus_app.moviefrumy_new.di.modules.RemoteModule
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        RemoteModule::class
+    ]
+)
+
 interface AppComponent {
 }
