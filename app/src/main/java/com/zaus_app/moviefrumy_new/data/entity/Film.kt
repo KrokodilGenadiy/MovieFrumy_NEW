eba3fd96c600923dd7fa4109d836675dcb7c5b19
KrokodilGenadiy <alex.usevich@gmail.com>
2023-01-02 19:01:30 +0300

    made Film entity parcelable

--- app/src/main/java/com/zaus_app/moviefrumy_new/data/entity/Film.kt
+++ app/src/main/java/com/zaus_app/moviefrumy_new/data/entity/Film.kt
@@ -1,9 +1,13 @@
 package com.zaus_app.moviefrumy_new.data.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Film(
     val title: String,
-    val poster: Int,
+    val poster: String,
     val description: String,
     var rating: Double = 0.0,
     var isInFavorites: Boolean = false
-)
+): Parcelable
