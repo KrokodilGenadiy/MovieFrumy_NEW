2cc31f18dc41478efd3249aacc5c698f66ad274a
KrokodilGenadiy <alex.usevich@gmail.com>
2023-01-03 15:38:29 +0300

    added rating view to film_item layout

--- app/src/main/java/com/zaus_app/moviefrumy_new/view/fragments/home_fragment/HomeFragment.kt
+++ app/src/main/java/com/zaus_app/moviefrumy_new/view/fragments/home_fragment/HomeFragment.kt
@@ -29,7 +29,6 @@ class HomeFragment : Fragment() {
         })
     }
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
